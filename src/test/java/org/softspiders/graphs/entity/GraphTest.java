4650f08e8df69a8638c035712998513d655e7db4
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-08-26 07:08:56 +0300

    feat: add initial Graph's test

---
+++ src/test/java/org/softspiders/graphs/entity/GraphTest.java
@@ -0,0 +1,16 @@
+package org.softspiders.graphs.entity;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.junit.Assert.*;
+
+public class GraphTest {
+
+	@Test
+	public void createByDefault() {
+		fail("Not implemented yet");
+	}
+
+}
